d24de3b0927f0a40070b5a21d7970af2cd59f8ed
master <master.code.develop@gmail.com>
2022-12-12 18:06:17 +0100

    DAJR-18 project architecture and addition of CSS

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,9 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
+  /*
+  TEST EXAMPLE:
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  */
 });
