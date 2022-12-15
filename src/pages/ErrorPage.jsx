884fdc8e49f7e598da559676a3b90d0403930b53
master <master.code.develop@gmail.com>
2022-12-15 02:08:07 +0100

    DAJR-21 update the Error page JSX: add components

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Header } from './Layout';
+import { Link } from 'react-router-dom';
 
 export default function ErrorPage() {
 	return(
 		<>
 			<Header />
+			<section className='error-page'>
+				<h1 className='error-page__title'>404</h1>
+				<p className='error-page__text'>Oups! La page que vous demandez n&apos;existe pas.</p>
+				<Link className='error-page__link' to="/">
+					Retourner sur la page d&apos;accueil
+				</Link>
+			</section>
 		</>
 	);
 }
