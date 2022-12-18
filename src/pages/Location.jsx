58d6782e6b7caf052d3974934e5a485a246f20bb
master <master.code.develop@gmail.com>
2022-12-18 15:40:44 +0100

    DAJR-25 retrieval of the id and call to the API

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
-
-export default function Location() {
+import React, { useEffect, useState } from 'react';
+import { getById } from '../functions/api';
+import { useParams } from 'react-router-dom';
+import Loading from '../components/Loading';
+export default function Location(props) {  
+	const params = useParams(),
+	[data, setData] = useState([]),
+	[loading, setLoading] = useState([]),
+	[error, setError] = useState(false),
+	$then = res => {
+		if (res.length !== 1) window.location.pathname='./error-404';
+		else {
+			setData(res);
+			setLoading(false)
+		}
+	},
+	$catch = err => {
+		console.log(err);
+		setError(err)
+		window.location.pathname='./error-404';
+	};
+	useEffect(()=>{
+		if (!error) {
+			getById(params.id,$then, $catch);
+		}
+	}, [error, params])
+	console.log(data);
 	return (
-		<div>Location</div>
+		<>
+			<Loading loading={loading}/>
+		</>
 	);
 }
